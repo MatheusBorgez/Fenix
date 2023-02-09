a38bed47fa40753b52504f651a94f6de2aba4f8f
MatheusBorgez <matheusab594@gmail.com>
2023-02-09 16:56:03 -0300

    Primeiro commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fenix.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
